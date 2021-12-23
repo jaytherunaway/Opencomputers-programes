99e078ce1602432c3a82027c0d174e93fe2acbb3
jaytherunaway <82423012+jaytherunaway@users.noreply.github.com>
2021-12-23 04:55:29 -0600

    Update update.lua

--- update.lua
+++ update.lua
@@ -2,3 +2,7 @@ print("getting BaseController")
 wget -f https://github.com/jaytherunaway/Opencomputers-programes/raw/main/BaseController
 print("")
 print("getting DroneController")
+https://github.com/jaytherunaway/Opencomputers-programes/raw/main/DC
+print("Update Complete")
+os.sleep(1)
+os.execute("clear")
